7c53fc34b98770f92d93b3115ace412834757593
Jonathas-bonfim <jonathas.santos.bonfim@gmail.com>
2022-04-04 22:02:26 -0300

    MirageJS and AXIOS

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { createServer } from 'miragejs';
 
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date(),
+        },
+        {
+          id: 2,
+          title: 'Transaction 2',
+          amount: 200,
+          type: 'deposit',
+          category: 'sport',
+          createdAt: new Date(),
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
